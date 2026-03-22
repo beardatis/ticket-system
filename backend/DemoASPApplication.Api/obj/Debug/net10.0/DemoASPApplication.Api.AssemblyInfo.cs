232d2f3df1633061ca7acb2418e8d4f71deb7a72
abognar <abognar@gmail.com>
2026-03-22 12:43:14 +0100

    Elso verzio: ticket rendszer

--- backend/DemoASPApplication.Api/obj/Debug/net10.0/DemoASPApplication.Api.AssemblyInfo.cs
+++ backend/DemoASPApplication.Api/obj/Debug/net10.0/DemoASPApplication.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoASPApplication.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5141595740a7f0e3b1a4b4079c2739571258e465")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoASPApplication.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoASPApplication.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
